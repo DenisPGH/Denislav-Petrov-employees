e62bba3781e333cf7b8554f9da4068422388d6be
Denislav <deni_9000_1990@abv.bg>
2022-05-03 17:20:50 +0300

    added today, separated calculation in the function

--- CSV/CSV/sirma/brack.py
+++ CSV/CSV/sirma/brack.py
@@ -19,3 +19,39 @@
 #
 #         context['files'] = list_show
 #         return context
+
+
+""" funk"""
+
+# """find the longest time in one project"""
+#     all_project_id = Employees.objects.filter().values_list('ProjectID', flat=True).distinct()
+#     list_all_project_id = list(all_project_id)
+#
+#     id_of_max_pair=0
+#     time_first_employees = 0
+#     time_second_employees = 0
+#
+#     current_max_time_together_work_on_same_project=0
+#     final_max_time=datetime.timedelta(1900,10,10,10,10,10,10)
+#     final_project_id=0
+#     final_emp_ids=[]
+#
+#
+#     for each_project_id in list_all_project_id:
+#         list_time_both_empl = []
+#         result_set=Employees.objects.filter(ProjectID=each_project_id)
+#         ids_of_current_pair=[x.EmpID for x in result_set]
+#         if len(result_set)==2:
+#             for each_emp in result_set:
+#                 # calculate time for current pair
+#                 time_working_on_project=abs(each_emp.DateFrom-each_emp.DateTo)
+#                 #print(f"{each_emp.EmpID} = {time_working_on_project}")
+#                 list_time_both_empl.append(time_working_on_project)
+#             current_max_time_together_work_on_same_project= max(list_time_both_empl) - min(list_time_both_empl)
+#             print(f"ProjectID: {each_project_id}= {current_max_time_together_work_on_same_project}( {ids_of_current_pair})")
+#             if current_max_time_together_work_on_same_project > final_max_time:
+#                 final_max_time=current_max_time_together_work_on_same_project
+#                 final_project_id=each_project_id
+#                 final_emp_ids=ids_of_current_pair
+#
+#
--- CSV/CSV/sirma/views.py
+++ CSV/CSV/sirma/views.py
@@ -12,59 +12,56 @@ from django.shortcuts import render
 # Create your views here.
 from CSV.sirma.models import CSVfiles, Employees
 
-class Helper:
-    pass
+def helper(all_project_id):
+    """find the longest time in one project"""
+    list_all_project_id = list(all_project_id)
+
+    id_of_max_pair = 0
+    time_first_employees = 0
+    time_second_employees = 0
+
+    final_max_time = datetime.timedelta(1900, 10, 10, 10, 10, 10, 10)
+    final_project_id = 0
+    final_emp_ids = []
+
+    for each_project_id in list_all_project_id:
+        current_max_time_together_work_on_same_project = 0
+        list_time_both_empl = []
+        result_set = Employees.objects.filter(ProjectID=each_project_id)
+        ids_of_current_pair = [x.EmpID for x in result_set]
+        if len(result_set) == 2:
+            for each_emp in result_set:
+                # calculate time for current pair
+                time_working_on_project = abs(each_emp.DateFrom - each_emp.DateTo)
+                list_time_both_empl.append(time_working_on_project)
+            current_max_time_together_work_on_same_project = \
+                max(list_time_both_empl) - min(list_time_both_empl)
+            if current_max_time_together_work_on_same_project > final_max_time:
+                final_max_time = current_max_time_together_work_on_same_project
+                final_project_id = each_project_id
+                final_emp_ids = ids_of_current_pair
+
+    return final_emp_ids,final_project_id,final_max_time
+
+
 
 
 
 class StartPage(views.CreateView):
-
     model = CSVfiles
     fields = '__all__'
     template_name = 'index.html'
-    """find the longest time in one project"""
-    all_project_id = Employees.objects.filter().values_list('ProjectID', flat=True).distinct()
-    list_all_project_id = list(all_project_id)
-
-    id_of_max_pair=0
-    time_first_employees = 0
-    time_second_employees = 0
-
-    current_max_time_together_work_on_same_project=0
-    final_max_time=datetime.timedelta(1900,10,10,10,10,10,10)
-    final_project_id=0
-    final_emp_ids=[]
-
-
-    for each_project_id in list_all_project_id:
-        list_time_both_empl = []
-        result_set=Employees.objects.filter(ProjectID=each_project_id)
-        ids_of_current_pair=[x.EmpID for x in result_set]
-        if len(result_set)==2:
-            for each_emp in result_set:
-                # calculate time for current pair
-                time_working_on_project=abs(each_emp.DateFrom-each_emp.DateTo)
-                #print(f"{each_emp.EmpID} = {time_working_on_project}")
-                list_time_both_empl.append(time_working_on_project)
-            current_max_time_together_work_on_same_project= max(list_time_both_empl) - min(list_time_both_empl)
-            print(f"ProjectID: {each_project_id}= {current_max_time_together_work_on_same_project}( {ids_of_current_pair})")
-            if current_max_time_together_work_on_same_project > final_max_time:
-                final_max_time=current_max_time_together_work_on_same_project
-                final_project_id=each_project_id
-                final_emp_ids=ids_of_current_pair
-
-
-
     success_url = reverse_lazy('index')
+
     def get_context_data(self, **kwargs):
+        all_project_id = Employees.objects.filter().values_list('ProjectID', flat=True).distinct()
+        ids_emp, id_project, duration = helper(all_project_id)
         """ clear the db for every new file"""
         delete_all_data = Employees.objects.all()
         delete_all_data.delete()
-
         context = super().get_context_data(**kwargs)
         file_to_show=CSVfiles.objects.all().last()
         all_emp=Employees.objects.all()
-
         """ store the all data to the db"""
         if file_to_show:
             with open(f'{file_to_show.file}', newline='') as csvfile:
@@ -77,7 +74,7 @@ class StartPage(views.CreateView):
                     date_to=row[3].strip()
 
                     if date_to=='NULL':
-                        date_to='2022-05-03'
+                        date_to=datetime.datetime.today() 
                     new_emp=Employees(
                         EmpID=emp,
                         ProjectID=project,
@@ -88,12 +85,139 @@ class StartPage(views.CreateView):
 
         """return the result ot render on html page"""
         context['employees'] = all_emp
-        context['result'] = self.list_all_project_id
-        context['result_set'] = self.result_set
-        context['longest_pair_project_id'] = self.final_project_id
-        context['duration'] = self.final_max_time
-        context['ids_winners'] = ','.join([str(x) for x in self.final_emp_ids])
+        context['longest_pair_project_id'] = id_project
+        context['duration'] = duration
+        context['ids_winners'] = ','.join([str(x) for x in ids_emp])
         return context
+
+
+
+
+
+
+
+
+# class StartPage(views.CreateView):
+#
+#     model = CSVfiles
+#     fields = '__all__'
+#     template_name = 'index.html'
+#     """find the longest time in one project"""
+#     all_project_id = Employees.objects.filter().values_list('ProjectID', flat=True).distinct()
+#     list_all_project_id = list(all_project_id)
+#
+#     id_of_max_pair=0
+#     time_first_employees = 0
+#     time_second_employees = 0
+#
+#     current_max_time_together_work_on_same_project=0
+#     final_max_time=datetime.timedelta(1900,10,10,10,10,10,10)
+#     final_project_id=0
+#     final_emp_ids=[]
+#
+#
+#     for each_project_id in list_all_project_id:
+#         list_time_both_empl = []
+#         result_set=Employees.objects.filter(ProjectID=each_project_id)
+#         ids_of_current_pair=[x.EmpID for x in result_set]
+#         if len(result_set)==2:
+#             for each_emp in result_set:
+#                 # calculate time for current pair
+#                 time_working_on_project=abs(each_emp.DateFrom-each_emp.DateTo)
+#                 #print(f"{each_emp.EmpID} = {time_working_on_project}")
+#                 list_time_both_empl.append(time_working_on_project)
+#             current_max_time_together_work_on_same_project= max(list_time_both_empl) - min(list_time_both_empl)
+#             print(f"ProjectID: {each_project_id}= {current_max_time_together_work_on_same_project}( {ids_of_current_pair})")
+#             if current_max_time_together_work_on_same_project > final_max_time:
+#                 final_max_time=current_max_time_together_work_on_same_project
+#                 final_project_id=each_project_id
+#                 final_emp_ids=ids_of_current_pair
+#
+#
+#
+#     success_url = reverse_lazy('index')
+#     def get_context_data(self, **kwargs):
+#         """ clear the db for every new file"""
+#         delete_all_data = Employees.objects.all()
+#         delete_all_data.delete()
+#
+#         context = super().get_context_data(**kwargs)
+#         file_to_show=CSVfiles.objects.all().last()
+#         all_emp=Employees.objects.all()
+#
+#         """ store the all data to the db"""
+#         if file_to_show:
+#             with open(f'{file_to_show.file}', newline='') as csvfile:
+#                 # chetene file
+#                 reader = csv.reader(csvfile)
+#                 for row in reader:
+#                     emp=row[0]
+#                     project=row[1]
+#                     date_from=row[2].strip()
+#                     date_to=row[3].strip()
+#
+#                     if date_to=='NULL':
+#                         date_to='2022-05-03'
+#                     new_emp=Employees(
+#                         EmpID=emp,
+#                         ProjectID=project,
+#                         DateFrom=date_from,
+#                         DateTo=date_to,
+#                     )
+#                     new_emp.save()
+#
+#         """return the result ot render on html page"""
+#         context['employees'] = all_emp
+#         context['result'] = self.list_all_project_id
+#         context['result_set'] = self.result_set
+#         context['longest_pair_project_id'] = self.final_project_id
+#         context['duration'] = self.final_max_time
+#         context['ids_winners'] = ','.join([str(x) for x in self.final_emp_ids])
+#         return context
+
+
+class ShowResult(views.TemplateView):
+    template_name = 'result.html'
+
+    def get_context_data(self, **kwargs):
+        all_project_id = Employees.objects.filter().values_list('ProjectID', flat=True).distinct()
+        ids_emp, id_project, duration = helper(all_project_id)
+        """ clear the db for every new file"""
+        delete_all_data = Employees.objects.all()
+        delete_all_data.delete()
+        context = super().get_context_data(**kwargs)
+        file_to_show = CSVfiles.objects.all().last()
+        all_emp = Employees.objects.all()
+
+        """ store the all data to the db"""
+        if file_to_show:
+            with open(f'{file_to_show.file}', newline='') as csvfile:
+                # chetene file
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    emp = row[0]
+                    project = row[1]
+                    date_from = row[2].strip()
+                    date_to = row[3].strip()
+
+                    if date_to == 'NULL':
+                        date_to = '2022-05-03'
+                    new_emp = Employees(
+                        EmpID=emp,
+                        ProjectID=project,
+                        DateFrom=date_from,
+                        DateTo=date_to,
+                    )
+                    new_emp.save()
+
+        """return the result ot render on html page"""
+        context['employees'] = all_emp
+        context['longest_pair_project_id'] = id_project
+        context['duration'] = duration
+        context['ids_winners'] = ','.join([str(x) for x in ids_emp])
+        return context
+
+    success_url = reverse_lazy('result')
 
 
 
